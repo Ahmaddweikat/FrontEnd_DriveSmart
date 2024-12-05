c51dee89a259f73e137d6061a914d19368a6cfd2
Ibraheem Qadi <ibra444hem@gmail.com>
2024-12-05 18:57:40 +0200

    Add LicenseTypes constant

---
+++ src/constants/licenseTypes.js
@@ -0,0 +1,10 @@
+const LicenseTypes = Object.freeze({
+  MOTORCYCLE: "Motorcycle License",
+  TRACTOR: "Tractor License",
+  PRIVATE: "Private Vehicle License",
+  LIGHT_COMMERCIAL: "Light Commercial Vehicle License",
+  HEAVY_COMMERCIAL: "Heavy Commercial Vehicle License",
+  PUBLIC_TRANSPORT: "Public Transport License",
+});
+
+export default LicenseTypes;
