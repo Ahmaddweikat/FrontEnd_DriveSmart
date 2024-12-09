ffc28f4f9196f2c309993a50d58fc8fb02e1ab99
Ibraheem Qadi <ibra444hem@gmail.com>
2024-12-09 13:06:12 +0200

    Remove background opacity from Spinner component

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -2,7 +2,7 @@ import { CircularProgress } from "@mui/material";
 
 const Spinner = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50">
+    <div className="fixed inset-0 flex items-center justify-center z-50">
       <CircularProgress sx={{ color: "#72b626" }} />
     </div>
   );
