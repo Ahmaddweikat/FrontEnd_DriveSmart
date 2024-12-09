3b1fa004081ab49582899ad9d0ed8dc1cf6a320f
Ahmad Dweikat <a.dwekat.1999@gmail.com>
2024-12-09 17:02:20 +0200

    add new form

--- src/pages/TheoryPage/data/personalLicense/index.js
+++ src/pages/TheoryPage/data/personalLicense/index.js
@@ -1,5 +1,7 @@
 import { questions as Form1Questions } from './Form1';
 import { questions as Form2Questions } from './Form2';
+import { questions as Form3Questions } from './Form3';
+
 
 export const personalLicenseForms = [
   {
@@ -8,7 +10,6 @@ export const personalLicenseForms = [
     title: "Form number 1",
     questions: Form1Questions,
     timeLimit: "40 minutes",
-    category: "Rules and Signs"
   },
   {
     id: 2,
@@ -16,6 +17,12 @@ export const personalLicenseForms = [
     title: "Form number 2",
     questions: Form2Questions,
     timeLimit: "40 minutes",
-    category: "Scenarios"
+  },
+  {
+    id: 3,
+    formType: 'form3',
+    title: "Form number 3",
+    questions: Form3Questions,
+    timeLimit: "40 minutes",
   }
 ];
