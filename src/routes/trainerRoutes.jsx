780d334f61c168aa54f5415c6169b88e9c359930
Ahmad Dweikat <a.dwekat.1999@gmail.com>
2024-12-10 20:48:47 +0200

    add student page and school page for trainer dashboard

--- src/routes/trainerRoutes.jsx
+++ src/routes/trainerRoutes.jsx
@@ -1,6 +1,11 @@
 import TrainerSignUp from "../pages/Trainer/SignUpPage"
+import StudentsPage from "../pages/Trainer/TrainerProfile/StudentsPage"
+import School from "../pages/SchoolPage";
+
 const trainerRoutes = [
     { path: "signup", element: <TrainerSignUp /> },
+    { path: "students", element: <StudentsPage /> },
+    { path: "School", element: <School /> },
 
 ];
 
